e22b1850c5f64cd41b091d7d6e3c3b6faec3a02b
PaulPetrash <paulpetrash1@gmail.com>
2022-06-21 18:02:53 +0300

    refactor auth controller

--- src/main/java/com/exceedit/auth/web/controller/AuthController.java
+++ src/main/java/com/exceedit/auth/web/controller/AuthController.java
@@ -1,85 +1,69 @@
 package com.exceedit.auth.web.controller;
 
-import com.exceedit.auth.model.Client;
-import com.exceedit.auth.model.User;
 import com.exceedit.auth.repository.ClientRepository;
 import com.exceedit.auth.repository.UserRepository;
 import com.exceedit.auth.web.dto.AuthParamsDto;
-import org.springframework.security.core.Authentication;
+import lombok.val;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 
 @RestController
 @RequestMapping(path = "oauth")
 public class AuthController {
 
-        @Autowired
-        private UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-        @Autowired
-        private ClientRepository clientRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-//        @Autowired
-//        private PasswordEncoder passwordEncoder;
+    @Autowired
+    private ClientRepository clientRepository;
 
-        @Resource(name="authenticationManager")
-        private AuthenticationManager authManager;
+    @Resource(name = "authenticationManager")
+    private AuthenticationManager authManager;
 
-//        private  static  final PasswordEncoder b = new BCryptPasswordEncoder();
+    @RequestMapping("/authorize")
+    public ModelAndView authorize(Model model) {
+        ModelAndView loginTemplate = new ModelAndView("login");
+        loginTemplate.addObject("code", "ALLO");
+        return loginTemplate;
+    }
 
-        @RequestMapping("/authorize")
-        public ModelAndView authorize(Model model) {
-                ModelAndView loginTemplate = new ModelAndView("login");
-                loginTemplate.addObject("code", "ALLO");
-                return loginTemplate;
+    @PostMapping(path = "/login",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ModelAndView login(AuthParamsDto params, final HttpServletRequest request) {
+
+        val user = userRepository.findByEmail(params.getEmail());
+
+        if (user == null) {
+            val loginTemplate = new ModelAndView("login");
+            return loginTemplate
+                    .addObject("code", params.getCode())
+                    .addObject("emailError", "User with this email was not found");
         }
 
-        @PostMapping(path = "/login",
-                consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-                produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-        public ModelAndView login(AuthParamsDto params, final HttpServletRequest request) {
+        val auth = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(params.getEmail(), params.getPassword()));
 
-                User user = userRepository.findByEmail(params.getEmail());
-                Client client = clientRepository.findByClientId(params.getClientId());
-                System.out.println("LOOOG" + params.getClientId());
-                System.out.println("LOOOG" + client.getName());
+        val sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
 
-                ModelAndView loginTemplate = new ModelAndView("login");
-                loginTemplate.addObject("code", params.getCode());
+        val session = request.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", sc);
 
-                if(user == null){
-                        loginTemplate.addObject("emailError", "User with this email was not found");
-                        return loginTemplate;
-                }
-//                if(!b.matches(params.getPassword(), user.getPassword())){
-//                        loginTemplate.addObject("passwordError", "Password Incorrect");
-//                        return loginTemplate;
-//                }
-                UsernamePasswordAuthenticationToken authReq =
-                        new UsernamePasswordAuthenticationToken(params.getEmail(), params.getPassword());
-                Authentication auth = authManager.authenticate(authReq);
-                SecurityContext sc = SecurityContextHolder.getContext();
-                sc.setAuthentication(auth);
-                HttpSession session = request.getSession(true);
-                session.setAttribute("SPRING_SECURITY_CONTEXT", sc);
-
-                String externalUrl = "https://google.com";
-                return new ModelAndView("redirect:" + "/");
-
-        }
+        return new ModelAndView("redirect:" + "/");
+    }
 
 }
